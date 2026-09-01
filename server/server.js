05e530178d603552eaf437ba026af441249b00bb
Roamnest-ai <roamnestbookings@gmail.com>
2026-09-01 14:23:36 +0300

    Use environment variable for server port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { Resend } = require("resend");
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
